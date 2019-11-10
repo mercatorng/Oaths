39b5710ad49e2326459ecfaba6d8352871c2b922
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-11-10 08:11:27 +0100

    commit

--- src/app/templates/age-declaration/age-declaration.component.ts
+++ src/app/templates/age-declaration/age-declaration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { InstitutionService } from './../../services/institution.service';
+import { User } from './../../models/user';
+import { Institution } from 'src/app/models/institution';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-age-declaration',
@@ -6,10 +10,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./age-declaration.component.scss']
 })
 export class AgeDeclarationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  currentUser: User;
+  institution: Institution;
+  ageForm: FormGroup;
+  submitted = false;
+  constructor(
+    private institutionService: InstitutionService,
+    private formBuilder: FormBuilder
+  ) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser')) as User;
   }
 
+  ngOnInit() {
+    this.getInstitution();
+    this.ageForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      age: [
+        '',
+        [Validators.required, Validators.minLength(1), Validators.maxLength(3)]
+      ],
+      dateofBirth: ['', Validators.required],
+      relationship: ['', Validators.required],
+      relativeName: ['', Validators.required],
+      birthStreet: ['', Validators.required],
+      birthLocalGovt: ['', Validators.required],
+      registeredLocalLGovt: ['', Validators.required],
+      registeredState: ['', Validators.required],
+      registeredCountry: ['', Validators.required],
+      date: ['', Validators.required],
+      courtName: ['', Validators.required],
+      fromLanguage: ['', Validators.required],
+      toLanguage: ['', Validators.required],
+      interpreter: ['', Validators.required],
+      amountPaid: ['', Validators.required],
+      tellerNumber: ['', Validators.required]
+    });
+    
+  }
+
+  get f() {
+    return this.ageForm.controls;
+  }
+  
+  get name() { return this.ageForm.get('name'); }
+  get address() { return this.ageForm.get('address'); }
+  get age() { return this.ageForm.get('age'); }
+  get dob() { return this.ageForm.get('dateofBirth'); }
+  getInstitution() {
+    this.institutionService
+      .getInstitutionById(this.currentUser.institutionID)
+      .subscribe(res => {
+        this.institution = res as Institution;
+      });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.ageForm.invalid) {
+      return;
+    }
+  }
+
+  reset() {
+    this.submitted = false;
+    this.ageForm.reset();
+  }
 }
--- src/app/templates/general-form/general-form.component.ts
+++ src/app/templates/general-form/general-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InstitutionService } from './../../services/institution.service';
+import { User } from './../../models/user';
+import { Institution } from 'src/app/models/institution';
 
 @Component({
   selector: 'app-general-form',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general-form.component.scss']
 })
 export class GeneralFormComponent implements OnInit {
-
-  constructor() { }
-
+  currentUser: User;
+  institution: Institution;
+  constructor(private institutionService: InstitutionService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser')) as User;
+  }
   ngOnInit() {
+    this.getInstitution();
   }
 
+  getInstitution() {
+    this.institutionService
+      .getInstitutionById(this.currentUser.institutionID)
+      .subscribe(res => {
+        this.institution = res as Institution;
+      });
+  }
 }
