d9580e50c29aefead372aefdc1c4dd0553c91852
Modupe Adebayo <modupeadebayo001@gmail.com>
2019-11-12 07:31:56 +0100

    install packages from adigun's merge

--- src/app/print/print.component.ts
+++ src/app/print/print.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { DocumentService } from '../services/document.service';
-import { MatDialog } from '@angular/material/dialog';
-import { MatDialogComponent } from '../mat-dialog/mat-dialog.component';
-import { AlertService } from '../services/alert.service';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { DocumentService } from "../services/document.service";
+import { MatDialog } from "@angular/material/dialog";
+import { MatDialogComponent } from "../mat-dialog/mat-dialog.component";
+import { AlertService } from "../services/alert.service";
 
 @Component({
-  selector: 'app-print',
-  templateUrl: './print.component.html',
-  styleUrls: ['./print.component.scss']
+  selector: "app-print",
+  templateUrl: "./print.component.html",
+  styleUrls: ["./print.component.scss"]
 })
 export class PrintComponent implements OnInit {
   searchForm: FormGroup;
@@ -25,39 +25,39 @@ export class PrintComponent implements OnInit {
 
   ngOnInit() {
     this.searchForm = new FormGroup({
-      search: new FormControl('', [Validators.required])
+      search: new FormControl("", [Validators.required])
     });
   }
   get search() {
-    return this.searchForm.get('search');
+    return this.searchForm.get("search");
   }
 
   openDialog(title, msg): void {
     this.dialog.open(MatDialogComponent, {
-      width: '250px',
+      width: "250px",
       data: { title, msg }
     });
   }
 
   getDocument() {
-    console.log(this.searchForm.get('search').value);
-    this.submitted = true;
-    if (this.searchForm.invalid) {
-      return;
-    }
-    this.loading = true;
-    this.documentService.getDocumentByRef(this.searchForm.value).subscribe(
-      res => {
-        this.affidavit = res;
-        console.log(this.searchForm.value);
-        this.openDialog('Court Affidavit', `Success`);
-        this.loading = false;
-      },
-      error => {
-        this.loading = false;
-        this.alertService.error(`Error: ${error.statustext}`);
-        this.openDialog('Court Affidavit', `Failed ${error.statusText}`);
-      }
-    );
+    console.log(this.searchForm.get("search").value);
+    // this.submitted = true;
+    // if (this.searchForm.invalid) {
+    //   return;
+    // }
+    // this.loading = true;
+    // this.documentService.getDocumentByRef(this.searchForm.value).subscribe(
+    //   res => {
+    //     this.affidavit = res;
+    //     console.log(this.searchForm.value);
+    //     this.openDialog('Court Affidavit', `Success`);
+    //     this.loading = false;
+    //   },
+    //   error => {
+    //     this.loading = false;
+    //     this.alertService.error(`Error: ${error.statustext}`);
+    //     this.openDialog('Court Affidavit', `Failed ${error.statusText}`);
+    //   }
+    // );
   }
 }
